78be74fed4ebdc1a0cf445c9e92fba7fe5fc7765
proxima812 <mrzuc2015@yandex.ru>
2023-12-09 00:45:13 +0600

    bot

---
+++ src/utils/getPageReflection.ts
@@ -0,0 +1,8 @@
+import { getCollection } from 'astro:content'
+
+export const getPageReflection = async (collection: any, moth: any) => {
+  const allPage = (await getCollection(collection))
+    .filter((a: any) => a.data.moth === moth)
+    .sort((a: any, b: any) => a.data.id.length - b.data.id.length)
+  return allPage
+}
